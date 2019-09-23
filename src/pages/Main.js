7ff8982a3af34ae797ac0943e2e44d4ef0b89265
Wesley Junior <wesley_jr40@msn.com>
2019-09-23 09:52:22 -0300

    - trocar socket.io-client do yarn pro npm - teste;

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,52 +2,52 @@
 // sempre que uma condição é satisfeita, toda vez que o valor de um parâmetro é modificado
 // quando não se informa nada como condicão, o useEffect é dispardo apenas um vez
 //no carregamento da página
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Main.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Main.css";
 
-import api from '../services/api';
+import api from "../services/api";
 
-import logo from '../assets/logo.svg';
-import like from '../assets/like.svg';
-import dislike from '../assets/dislike.svg';
-import itsamatch from '../assets/itsamatch.png';
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import itsamatch from "../assets/itsamatch.png";
 
 //parte do websocket
-import socket from 'socket.io-client';
+import socket from "socket.io-client";
 
 //sobre propriedades da rota ver mais no Login.js
 //match = contém dados sobre a rota, parametros etc
 export default function Main({ match }) {
-    const [users, setUsers] = useState([]);
-    const [matchDev, setMatchDev] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
-    useEffect(() => {
-        async function loadUsers() {
-            const response = await api.get('/devs', {
-                headers: {
-                    user: match.params.id
-                }
-            });
-
-            console.log(response.data);
-            setUsers(response.data);
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
         }
+      });
 
-        loadUsers();
-    }, [match.params.id]);
+      console.log(response.data);
+      setUsers(response.data);
+    }
 
-    useEffect(() => {
-        const con = socket('https://api-tindev.azurewebsites.net', {
-            query: { user: match.params.id }
-        });
+    loadUsers();
+  }, [match.params.id]);
 
-        con.on('match', result => {
-            console.log(result);
-            setMatchDev(result);
-        });
+  useEffect(() => {
+    const con = socket("https://api-tindev.azurewebsites.net", {
+      query: { user: match.params.id }
+    });
 
-        /*
+    con.on("match", result => {
+      console.log(result);
+      setMatchDev(result);
+    });
+
+    /*
         //Escutar mensagens do backend
         con.on('e ai?', result=>{
             console.log(`Backend message: ${JSON.stringify(result)}`);
@@ -59,94 +59,82 @@ export default function Main({ match }) {
             })
         }, 2000);
         */
+  }, [match.params.id]);
 
-    }, [match.params.id]);
+  async function handleLike(id) {
+    console.log(`like ${id}`);
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
 
-    async function handleLike(id) {
-        console.log(`like ${id}`);
-        await api.post(`/devs/${id}/likes`, null, {
-            headers: {
-                user: match.params.id
-            }
-        });
+    //para atualizar a pagina apos a ação de like ou dislike
+    //será modificado o conteudo da 'users', utilizando o 'setUsers'
+    //é necessário alterar os dados via 'setUsers' para que haja
+    // a alteração do estado do controle
+    setUsers(users.filter(u => u._id != id));
+  }
 
-        //para atualizar a pagina apos a ação de like ou dislike
-        //será modificado o conteudo da 'users', utilizando o 'setUsers'
-        //é necessário alterar os dados via 'setUsers' para que haja
-        // a alteração do estado do controle
-        setUsers(users.filter(u => u._id != id));
-    }
+  async function handleDislike(id) {
+    console.log(`dislike ${id}`);
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
 
-    async function handleDislike(id) {
-        console.log(`dislike ${id}`);
-        await api.post(`/devs/${id}/dislikes`, null, {
-            headers: {
-                user: match.params.id
-            }
-        });
+    //para atualizar a pagina apos a ação de like ou dislike
+    //será modificado o conteudo da 'users', utilizando o 'setUsers'
+    //é necessário alterar os dados via 'setUsers' para que haja
+    // a alteração do estado do controle
+    setUsers(users.filter(u => u._id != id));
+  }
 
-        //para atualizar a pagina apos a ação de like ou dislike
-        //será modificado o conteudo da 'users', utilizando o 'setUsers'
-        //é necessário alterar os dados via 'setUsers' para que haja
-        // a alteração do estado do controle
-        setUsers(users.filter(u => u._id != id));
-    }
-
-    return (
-        <div className="main-container">
-            <Link to="/">
-                <img src={logo} alt="Tindev Logo"></img>
-            </Link>
-            {
-                users && users.length > 0 ?
-                    (
-                        <ul>
-                            {
-                                /* () permitem renderizar os doms, 
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev Logo"></img>
+      </Link>
+      {users && users.length > 0 ? (
+        <ul>
+          {/* () permitem renderizar os doms, 
                                 ao inves de colocar um 'return' na 'function' */
-                                users.map(user => (
+          users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name}></img>
+              <footer>
+                <strong>{user.name}</strong>
+                <p>
+                  <small>{user.user}</small>
+                </p>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike"></img>
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like"></img>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Acabou :[</div>
+      )}
 
-                                    <li key={user._id}>
-                                        <img src={user.avatar} alt={user.name}></img>
-                                        <footer>
-                                            <strong>{user.name}</strong>
-                                            <p><small>{user.user}</small></p>
-                                            <p>{user.bio}</p>
-                                        </footer>
-                                        <div className="buttons">
-                                            <button type="button"
-                                                onClick={() => handleDislike(user._id)}>
-                                                <img src={dislike} alt="Dislike"></img>
-                                            </button>
-                                            <button type="button"
-                                                onClick={() => handleLike(user._id)}>
-                                                <img src={like} alt="Like"></img>
-                                            </button>
-                                        </div>
-                                    </li>
-
-                                ))
-                            }
-                        </ul>
-                    ) : (
-                        <div className="empty">
-                            Acabou :[
-                    </div>
-                    )
-            }
-
-            {
-                matchDev && (
-                    <div className="match-container">
-                        <img src={itsamatch} alt="It's a match"></img>
-                        <img className="avatar" src={matchDev.avatar} alt=""></img>
-                        <strong>{matchDev.name}</strong>
-                        <small>{matchDev.username}</small>
-                        <p>{matchDev.bio}</p>
-                        <button onClick={() => setMatchDev(null)}>Fechar</button>
-                    </div>
-                )
-            }
+      {matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="It's a match"></img>
+          <img className="avatar" src={matchDev.avatar} alt=""></img>
+          <strong>{matchDev.name}</strong>
+          <small>{matchDev.username}</small>
+          <p>{matchDev.bio}</p>
+          <button onClick={() => setMatchDev(null)}>Fechar</button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'http://localhost:3334'
-    
-    baseURL: 'https://api-tindev.azurewebsites.net'
+  //baseURL: 'http://localhost:3334'
+  baseURL: "https://api-tindev.azurewebsites.net"
 });
 
 export default api;
